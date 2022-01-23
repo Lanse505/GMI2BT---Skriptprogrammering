bb6b83abf7066fdbc4fdf934391a6c67ad5539b7
Lanse <simon_stalnabb@hotmail.com>
2022-01-23 22:36:00 +0100

    Push labb 2 code

--- Labbar/Labb-1-2/Labb-2/program.py
+++ Labbar/Labb-1-2/Labb-2/program.py
@@ -1,15 +1,64 @@
-from util import project_json as json
-from util import project_csv as csv
-from util import project_validation as validation
+import os.path
+import json
+import util
 
-if __name__ == "__main__":
-    target_csv = input("Please enter the path for your targetted csv: ")
-    while not validation.validate_file_path(target_csv):
-        print("Error: Invalid File Path, Make sure to provide a valid filepath to your csv!")
-        target_csv = input("Please enter the path for your targetted csv: ")
-    csv_reader = csv.openCSVFile(target_csv, 'r+')
-    temp_json = json.openJSONFile("/temp.json" 'r+')
+# TODO: Code Review with Joakim
+if __name__ == '__main__':  # Checks if the code is being invoked directly and not as an import
+    # Main Menu as a printable multi-line string
+    menu = """
+#####################
+# 1 - Add Person    #
+# 2 - Remove Person #
+# 3 - Save File     #
+# 4 - View Person   #
+# 5 - Exit          #
+#####################
+            """
+    field_names = ['username', 'first_name', 'last_name', 'email']  # Default Field Names
+    field_names_sv = ['användarnamn', 'förnamn', 'efternamn', 'epost'] # Default Swedish Field Names
 
-    
+    # Get if the user wants to load from the target CSV or the cached JSON
+    get_from_csv = input("Do you want to load from a CSV or from the cached JSON? [CSV, JSON]: ")
+    csv_dict = {}  # Setups an empty dict to use
+    if get_from_csv.upper() == "CSV":  # Checks if the user wanted to load from the CSV
+        util.populate_from_csv(csv_dict, field_names)  # Populates the csv_dict from the csv
+    if get_from_csv.upper() == "JSON" and os.path.exists('./temp.json'):  # Checks if the JSON file already exists
+        with open('./temp.json', "r+", encoding='utf-8-sig') as temp:  # Opens the existing temp.json
+            csv_dict = json.load(temp)  # Parses the JSON into the csv_dict as a dict object
+    with open('./temp.json', "w+", encoding='utf-8-sig') as temp:  # Opens a new file
+        json.dump(csv_dict, temp, indent=True, ensure_ascii=False)  # Dumps the dict to Json
 
+    is_first_run = True  # Sets the "first run" value so it only prints the json to log once
+    while True:  # Continuous Loop that's broken out of later
+        option = -1  # Checks if the current target is the menu or a sub-program
+        if is_first_run:  # If it is the first run
+            with open('./temp.json', "r+", encoding='utf-8-sig') as temp:  # Opens up the temp.json
+                print(temp.read())  # Prints the entire file contents to log
+            print() # Print empty line
+            is_first_run = False  # Set that this is no longer the first run of the main menu
 
+        while option == -1:  # If the option value is -1 then run the main menu cycle
+            print(menu)  # Prints the Main Menu multi-line string
+            valueIn = input("Select your preferred option: ")  # Asks for the preferred program option
+            # Checks if the input is a parsable integer and is within 1-5
+            while not util.is_integer(valueIn) or not util.is_within(int(valueIn), 1, 5):
+                print("Error: Invalid Input, Please enter a number between 1-4") # Prints error message if not
+                valueIn = input("Select your preferred option: ")  # Asks for new input and loops
+            option = int(valueIn)  # Parses the validated input to the option value
+
+        if option == 1:  # option 1: Add Person
+            option = util.addPerson(csv_dict) # Calls addPerson in util
+            input("Press Any Key to Return to Menu")  # Input to not instantly clear the message line
+        elif option == 2:  # option 2: Remove Person
+            option = util.removePerson(False, csv_dict) # Calls removePerson with a default "False" for while-loop
+            input("Press Any Key to Return to Menu")  # Input to not instantly clear the message line
+        elif option == 3:  # option 3: Flush JSON to CSV
+            print("Flushing JSON to CSV")  # Info Message
+            option = util.flush_to_csv(csv_dict)  # Calls flush_to_csv in util
+            input("Press Any Key to Return to Menu")  # Input to not instantly clear the message line
+        elif option == 4:  # option 4: runs the get_user_data function in util
+            option = util.get_user_data(csv_dict)  # Calls the get_user_data function in util
+            input("Press Any Key to Return to Menu")  # Input to not instantly clear the message line
+        elif option == 5:  # option 5: exit
+            break  # Breaks out of the while loop
+        print()  # Prints an empty line
---
+++ Labbar/Labb-1-2/Labb-2/util.py
@@ -0,0 +1,127 @@
+import csv
+import json
+import re
+
+
+# Opens up the csv as a file and returns both the file itself and the csv-reader for the file
+def open_csv(path: str, mode: str):
+    file = open(path, mode, encoding='utf-8-sig')  # Opens up the file using utf-8-sig encoding to support all chars
+    reader = csv.reader(file, delimiter=';')  # Opens up the CSV using a csv reader using ';' as a delimiter
+    return reader, file  # Returns a tuple of the csv reader and the original file
+
+
+# TODO: Code Review with Joakim
+# TODO: Rewrite to support any input location
+# Populates the csv_dict object using the clean csv file
+def populate_from_csv(csv_dict, field_names):
+    csv_file, file = open_csv("./clean.csv", "r+")  # Opens the csv
+    for i, row in enumerate(csv_file):  # Iterate over the rows in the csv
+        if i == 0:  # If the row is the header row then continue
+            continue  # Skip if it's the head column
+        person_dict = {}  # Create an empty dictionary for the user
+        for j, name in enumerate(field_names):  # Loops over the field_names
+            row_value = row[j]  # Get the value from the CSV
+            if row_value is not None:  # If the value isn't empty then add it and its value to the new dict
+                person_dict[name] = row[j]  # Set the fields of the "person_dict"
+                continue  # Continue as to not add double values to the dict
+            person_dict[name] = None  # If the value was empty then add an empty field value to the dict
+        csv_dict[row[0]] = person_dict  # Set the "person_dict" to the "csv_dict" with the username as key.
+    file.close()  # Closes the file reference
+
+
+# TODO: Code Review with Joakim
+# Flushes the code from the stored object map to JSON and the csv
+def flush_to_csv(csv_dict):
+    with open('./temp.json', "w", encoding='utf-8-sig') as clear:  # Clear the json file
+        pass
+    with open('./temp.json', "r+", encoding='utf-8-sig') as flush:
+        json.dump(csv_dict, flush, indent=True, ensure_ascii=False)
+    with open('./temp.json', "r+", encoding='utf-8-sig') as saved:
+        csv_dict = json.load(saved)
+        with open('./labb2_personer_vt22.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['användarnamn', 'förnamn', 'efternamn', 'epost'])
+            for (key, value) in enumerate(csv_dict.items()):
+                nk, nv = value
+                writer.writerow([nv['username'], nv['first_name'], nv['last_name'], nv['email']])
+    return -1
+
+
+# TODO: Code Review with Joakim
+# Adds a new person to the csv dictionary, also applies regex input validation.
+def addPerson(csv_dict):
+    username = input("Please enter a username for the person you want to add: ")
+    while not is_regex_compliant(username, r"[hv]\d{2}[a-z]{5}") and username not in csv_dict:
+        print("Invalid input: Please provide a valid username matching the following regex format:")
+        print(r"[[hv]\d{2}[a-z]{5}] - [Example: h00andan]")
+        username = input("Please enter a username for the person you want to add: ")
+    first_name = input("Please enter the first name of the user you want to add: ")
+    while first_name is None or first_name == "":
+        print("Error: Invalid Input - 'First Name' was either of type None or Empty")
+        first_name = input("Please enter the first name of the user you want to add: ")
+    last_name = input("Please enter the last name of the user you want to add: ")
+    while last_name is None or last_name == "":
+        print("Error: Invalid Input - 'First Name' was either of type None or Empty")
+        last_name = input("Please enter the last name of the user you want to add: ")
+    user_dict = {'username': username, 'first_name': first_name,
+                 'last_name': last_name, 'email': username + "@du.se"
+                 }
+    csv_dict[username] = user_dict
+    return -1
+
+
+# TODO: Code Review with Joakim
+# Removes a person from the list, validates the input username by checking if the dict contains the name.
+def removePerson(removed, csv_dict):
+    while not removed:
+        username = input("Please enter the username of the User you want to delete: ")
+        if username == "exit":
+            return -1
+        if username in csv_dict:
+            csv_dict.pop(username)
+            print(f"Removed user: {username}")
+            removed = True
+        else:
+            print(f"Error: User with Username: {username} , Didn't exist!")
+
+
+# TODO: Code Review with Joakim
+# Gets the userdata for the provided username, uses both regex validation and existence validation to validate the input
+def get_user_data(csv_data):
+    username = input("Please enter the username of the user you want to view: ")
+    while not is_regex_compliant(username, r"[hv]\d{2}[a-z]{5}") or username not in csv_data:
+        print(r"Invalid Error: Please input a regex compliant username [[hv]\d{2}[a-z]{5}]: ")
+        username = input("Please enter the username of the user you want to view: ")
+    data = csv_data[username]
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    print(f"Printing data for user: {username}")
+    print(f"Username: {username}")
+    print(f"First Name: {first_name}")
+    print(f"Last Name: {last_name}")
+    print(f"Email: {email}")
+    return -1
+
+
+# Checks if the provided input can be converted to an integer
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+# Checks if the provided string is valid against the provided regex pattern
+def is_regex_compliant(testable: str, pattern: str):
+    if re.match(pattern, testable):
+        return True
+    return False
+
+
+# Checks if the provided value is less than the maximum specified value but greater than the minimum specified value.
+def is_within(value, minimum_inclusive, maximum_inclusive):
+    if minimum_inclusive <= value <= maximum_inclusive:
+        return True
+    return False
